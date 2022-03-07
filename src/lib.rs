3dcc470baff5e28a95a34b1b2ef8023c9f3902ac
CREESTL <kroymw3@yandex.ru>
2022-03-07 19:39:54 +0300

    Implement s Simple Tree With Arena Allocation and Visitor Pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,153 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#![allow(dead_code)]
+
+// Export structs
+pub use node::Node;
+pub use tree::Tree;
+pub use iter::GraphIter;
+
+
+// Module of a graph node
+mod node {
+    // Alias for vector index
+    pub type Index = usize;
+
+    // Struct of a graph node
+    pub struct Node {
+        // Node has a value and other nodes connected to it
+        pub value: usize,
+        pub left: Option<Index>,
+        pub right: Option<Index>
+    }
+
+    impl Node {
+        // Constructor for a new node
+        pub fn new(value: usize, left: Option<Index>, right: Option<Index>) -> Self{
+            Node{value, left, right}
+        }
     }
 }
+
+
+// Module of a graph
+mod tree {
+    use super::node::{Node, Index};
+    use super::iter::GraphIter;
+
+    // Struct of a graph
+    pub struct Tree {
+        // Graph has a root and an arena
+        // Arena is a vector holding nodes of a graph. Allows for random access without nested borrowing
+        root: Option<Index>,
+        arena: Vec<Option<Node>>
+    }
+
+    impl Tree{
+        // Constructor of a graph
+        // At first, tree has no root. It must be set with set_root()
+        pub fn new() -> Self {
+            Tree{arena: vec![], root: None}
+        }
+
+        // Function returns a custom iterator over the graph
+        pub fn iter(&mut self) -> GraphIter {
+            GraphIter::new(self.root)
+        }
+
+        // Function sets the root of a graph
+        pub fn set_root(&mut self, root: Option<Index>) {
+            self.root = root;
+        }
+
+        // Function adds a node to the graph
+        // The index of that node in arena is returned
+        pub fn add_node(&mut self, node: Node) -> Index{
+            let index = self.arena.len();
+            self.arena.push(Some(node));
+            return index
+        }
+
+        // Function removes a node from the graph
+        pub fn remove_node(&mut self, index: Index) -> Option<Node> {
+            if let Some(node) = self.arena.get_mut(index) { 
+                node.take()
+            } else {
+                None
+            }
+        }
+
+        // Function gets the node from the graph (borrows it)
+        pub fn get_node(&self, index: Index) -> Option<&Node> {
+            if let Some(node) = self.arena.get(index) {
+                node.as_ref()
+            } else {
+                None
+            }
+        }
+
+        // Function gets a mutable node from the graph (mutably borrows it)
+        pub fn get_node_mut(&mut self, index: Index) -> Option<&mut Node> {
+            if let Some(node) = self.arena.get_mut(index) {
+                node.as_mut()
+            } else {
+                None
+            }
+        }
+
+
+
+    }
+}
+
+
+// Module of a custom iterator
+// Built-in Iterator trait doesn't fit current task. It can't mutably borrow an element of a vector if
+// the vector has been borrowed already
+mod iter{
+    use super::node::Index;
+    use super::tree::Tree;
+
+    pub struct GraphIter<> {
+        // Iterator holds all data in the stack
+        stack: Vec<Index>
+    }
+
+    impl GraphIter {
+        // Constructor of the iterator 
+        // It might take the index of the root to start iteration from
+        pub fn new(root: Option<Index>) -> Self {
+            if let Some(index) = root {
+                GraphIter {
+                    stack: vec![index]
+                }
+            } else {
+                GraphIter {
+                    stack: vec![]
+                }
+            }
+        }
+
+        // Function returns the next item from the iterator
+        // This function implements a Visitor Pattern. It only borrows a graph when it's beeing called
+        // Between the calls the graph can be modified in any way. A graph to borrow is passed as the second parameter
+        pub fn next(&mut self, tree: &Tree) -> Option<Index> {
+            // Get the next index from the stack
+            while let Some(node_index) = self.stack.pop(){
+                // Get the node with that index from the graph (from arena)
+                if let Some(node) = tree.get_node(node_index) {
+                    // Add it's neighbours to the stack
+                    if let Some(right) = node.right {
+                        self.stack.push(right);
+                    }
+                    if let Some(left) = node.left {
+                        self.stack.push(left);
+                    }
+
+                    return Some(node_index)
+                }
+            }
+
+            return None
+        }
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main(){
-	println!("here we go!");
+mod lib;
+use lib::{Node, Tree};
+
+fn main() {
+	// Create a simple tree with 5 nodes
+	let mut tree = Tree::new();
+	let a = tree.add_node(Node::new(4, None, None));
+	let b = tree.add_node(Node::new(5, None, None));
+	let c = tree.add_node(Node::new(2, Some(a), Some(b)));
+	let d = tree.add_node(Node::new(3, None, None));
+	let e = tree.add_node(Node::new(1, Some(c), Some(d)));
+
+	// Set the root of a tree
+	tree.set_root(Some(e));
+
+	// Create an iterator of a tree
+	let mut tree_iter = tree.iter();
+	// Iterate over the tree and print it's nodes
+	while let Some(i) = tree_iter.next(&tree) {
+		let node = tree.get_node(i).expect("No suck node!");
+		println!("{}", node.value);
+	}
 }
